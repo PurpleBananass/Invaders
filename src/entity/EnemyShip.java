c70aeeb74c89372e2bd5b61c1c92ba7d962dbc2b
Oh Jinwoo <oh7895@hanyang.ac.kr>
2023-10-05 20:39:05 +0900

    Feat: Create Item Object
    Item, ItemPool 객체 생성
    EnemyShip에 Item 보유 여부 Boolean 변수 추가

--- src/entity/EnemyShip.java
+++ src/entity/EnemyShip.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.Color;
+import java.util.Random;
 
 import engine.Cooldown;
 import engine.Core;
@@ -23,12 +24,17 @@ public class EnemyShip extends Entity {
 	/** Point value of a bonus enemy. */
 	private static final int BONUS_TYPE_POINTS = 100;
 
+	private static final double ITEM_PROPORTIOIN = 0.1;
+
+
 	/** Cooldown between sprite changes. */
 	private Cooldown animationCooldown;
 	/** Checks if the ship has been hit by a bullet. */
 	private boolean isDestroyed;
 	/** Values of the ship, in points, when destroyed. */
 	private int pointValue;
+
+	private boolean hasItem;
 
 	/**
 	 * Constructor, establishes the ship's properties.
@@ -149,4 +155,23 @@ public class EnemyShip extends Entity {
 	public final boolean isDestroyed() {
 		return this.isDestroyed;
 	}
+
+
+	/**
+	 * Enemyship에 랜덤으로 아이템 생성*/
+	private boolean itemGenerator(){
+		int bound = 10000;
+		int rand_int = new Random().nextInt(bound);
+		if(rand_int < (int)(bound * ITEM_PROPORTIOIN))
+			return true;
+		else
+			return false;
+	}
+
+	/** 적이 아이템을 지닌 객체인지 확인 */
+	public final boolean hasItem(){
+		return this.hasItem;
+	}
 }
+
+
