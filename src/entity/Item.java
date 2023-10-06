b11305883ea6b0c82b1a0e7e18b0d4791b226dad
Oh Jinwoo <oh7895@hanyang.ac.kr>
2023-10-06 17:21:05 +0900

    Feat: Implement Item Object
    Item, ItemPool을 구현했다.

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -1,4 +1,60 @@
 package entity;
 
-public class Item {
+import java.awt.Color;
+
+import engine.DrawManager.SpriteType;
+
+/**
+ * Implements an item that moves vertically only down.
+ */
+public class Item extends Entity {
+
+    /**
+     * Speed of the item, only positive number
+     * positive is down.
+     */
+    private int speed;
+
+    private boolean isGet;
+
+    /**
+     * Constructor, establishes the item's properties.
+     *
+     * @param positionX
+     *            Initial position of the item in the X axis.
+     * @param positionY
+     *            Initial position of the item in the Y axis.
+     */
+    public Item(final int positionX, final int positionY) {
+        super(positionX, positionY, 30, 30, Color.YELLOW);
+        this.speed = 50;
+        setSprite();
+    }
+
+    /**
+     * Sets correct sprite for the item, based on speed.
+     */
+    public final void setSprite() {
+        if (this.isGet)
+            this.spriteType = SpriteType.DroppedItem;
+        else
+            this.spriteType = SpriteType.AcquiredItem;
+    }
+
+    /**
+     * Updates the item's position.
+     */
+    public final void update() {
+        this.positionY += this.speed;
+    }
+
+
+    /**
+     * Getter for the speed of the item.
+     *
+     * @return Speed of the item.
+     */
+    public final int getSpeed() {
+        return this.speed;
+    }
 }
--- src/entity/ItemPool.java
+++ src/entity/ItemPool.java
@@ -1,4 +1,38 @@
 package entity;
 
-public class ItemPool {
+import java.util.HashSet;
+import java.util.Set;
+
+
+public final class ItemPool {
+
+
+    private static Set<Item> pool = new HashSet<Item>();
+
+
+    private ItemPool() {
+
+    }
+
+
+    public static Item getItem(final int positionX, final int positionY) {
+        Item item;
+        if (!pool.isEmpty()) {
+            item = pool.iterator().next();
+            pool.remove(item);
+            item.setPositionX(positionX - item.getWidth() / 2);
+            item.setPositionY(positionY);
+            item.setSprite();
+        } else {
+            item = new Item(positionX, positionY);
+            item.setPositionX(positionX - item.getWidth() / 2);
+        }
+        return item;
+    }
+
+
+    public static void recycle(final Set<Item> item) {
+        pool.addAll(item);
+    }
 }
+
