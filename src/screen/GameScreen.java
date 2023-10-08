86d057573a3ec86ee8e330bd97e97554d26b5472
thwldus <gee906@naver.com>
2023-10-09 01:53:36 +0900

    Life reduction due to escape of special enemy depending on level

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -70,6 +70,7 @@ public class GameScreen extends Screen {
 	private boolean levelFinished;
 	/** Checks if a bonus life is received. */
 	private boolean bonusLife;
+	private int escapeCnt = 0;
 
 	/**
 	 * Constructor, establishes the properties of the screen.
@@ -186,8 +187,27 @@ public class GameScreen extends Screen {
 			}
 			if (this.enemyShipSpecial != null
 					&& this.enemyShipSpecial.getPositionX() > this.width) {
+				this.escapeCnt++;
+				if(this.level==7){
+					this.lives--;
+					this.logger.info("This level is 7 and escaped ship is 1, so you lost on life.");
+				}
+				else if(this.level==6 && this.escapeCnt==2){
+					this.lives--;
+					this.logger.info("Escaped 2.");
+					this.escapeCnt = 0;
+					this.logger.info("This level is 6 and escaped ship is 2, so you lost on life.");
+				}
+				else if(this.level==5 && this.escapeCnt==3){
+					this.lives--;
+					this.logger.info("Escaped 3.");
+					this.escapeCnt = 0;
+					this.logger.info("This level is 5 and escaped ship is 3, so you lost on life.");
+				}
+				else{
+					this.logger.info("The special ship has escaped");
+				}
 				this.enemyShipSpecial = null;
-				this.logger.info("The special ship has escaped");
 			}
 
 			this.ship.update();
