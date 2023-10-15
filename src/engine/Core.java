3eb52b1504cf3427c6b690e6fe89d4c7a70fe74a
Hojin Go <hojin1760@gmail.com>
2023-10-15 20:42:44 +0900

    Temporarily connect 1P MODE

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -116,7 +116,7 @@ public final class Core {
 		do {
 			// TODO 1P mode와 2P mode 진입 구현
 			// TODO gameState 생성자에 따라 1P와 2P mode 구분
-			if(gameMode == 1) gameState = new GameState(1, 0, MAX_LIVES, 0, 0);
+			if(SelectScreen.gameMode == 1) gameState = new GameState(1, 0, MAX_LIVES, 0, 0);
 			else gameState = new GameState(1, 0, MAX_LIVES, MAX_LIVES, 0, 0, 0);
 
 			switch (returnCode) {
