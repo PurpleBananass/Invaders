182aa488c6fd73826d626b10bb1f3c78397bb984
brightalot <gamja9151@hanyang.ac.kr>
2023-10-16 17:52:54 +0900

    feat : acurracy 2p 분리를 위한 플레이어 별 bullet 재사용을 위한 map 적용

--- src/entity/BulletPool.java
+++ src/entity/BulletPool.java
@@ -1,6 +1,8 @@
 package entity;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -12,7 +14,7 @@ import java.util.Set;
 public final class BulletPool {
 
 	/** Set of already created bullets. */
-	private static Set<Bullet> pool = new HashSet<Bullet>();
+	private static Map<Integer, Set<Bullet>> pool = new HashMap<Integer, Set<Bullet>>();
 
 	/**
 	 * Constructor, not called.
@@ -32,14 +34,17 @@ public final class BulletPool {
 	 * @param speed
 	 *            Requested speed of the bullet, positive or negative depending
 	 *            on direction - positive is down.
+	 * @param shooter
+	 *            Player who shot the bullet.
+	 *            0 for enemy, 1 for first player, 2 for second player.
 	 * @return Requested bullet.
 	 */
 	public static Bullet getBullet(final int positionX,
-			final int positionY, final int speed) {
+			final int positionY, final int speed, final int shooter) {
 		Bullet bullet;
-		if (!pool.isEmpty()) {
-			bullet = pool.iterator().next();
-			pool.remove(bullet);
+		if (!pool.containsKey(shooter) && !pool.get(shooter).isEmpty()) {
+			bullet = pool.get(shooter).iterator().next();
+			pool.get(shooter).remove(bullet);
 			bullet.setPositionX(positionX - bullet.getWidth() / 2);
 			bullet.setPositionY(positionY);
 			bullet.setSpeed(speed);
@@ -47,7 +52,7 @@ public final class BulletPool {
 			// Will add a function to select the Bigger version and the base version later.
 //			bullet.setBiggerSprite();
 		} else {
-			bullet = new Bullet(positionX, positionY, speed);
+			bullet = new Bullet(positionX, positionY, speed, shooter);
 			bullet.setPositionX(positionX - bullet.getWidth() / 2);
 		}
 		return bullet;
@@ -56,10 +61,16 @@ public final class BulletPool {
 	/**
 	 * Adds one or more bullets to the list of available ones.
 	 * 
-	 * @param bullet
+	 * @param bullets
 	 *            Bullets to recycle.
 	 */
-	public static void recycle(final Set<Bullet> bullet) {
-		pool.addAll(bullet);
+	public static void recycle(final Set<Bullet> bullets) {
+		for (Bullet bullet : bullets) {
+			int shooter = bullet.getShooter();
+			if (!pool.containsKey(shooter)) {
+				pool.put(shooter, new HashSet<Bullet>());
+			}
+			pool.get(shooter).add(bullet);
+		}
 	}
 }
