f64558089fed8957d24f42c4ea4846ab0049f8e1
Oh Jinwoo <oh7895@hanyang.ac.kr>
2023-10-21 21:17:33 +0900

    Fix: Dead Plane Item Store Bug
    죽은 플레이어가 아이템을 먹는 버그 해결

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -1010,7 +1010,7 @@ public class GameScreen extends Screen {
 		Set<Item> recyclableItem = new HashSet<Item>();
 
 		for (Item item : this.items) {
-			if (checkCollision(item, this.ship) && !this.levelFinished) {
+			if (checkCollision(item, this.ship) && !this.levelFinished && this.lives != 0) {
 				recyclableItem.add(item);
 				SoundManager.playSound("SFX/S_Item_Get", "ItemGet", false, false);
 				this.ship.getItemQueue().enque(item);
@@ -1018,7 +1018,7 @@ public class GameScreen extends Screen {
 		}
 		if (gameState.getMode() == 2) {
 			for (Item item : this.items) {
-				if (checkCollision(item, this.ship2) && !this.levelFinished) {
+				if (checkCollision(item, this.ship2) && !this.levelFinished && this.lives2 != 0) {
 					recyclableItem.add(item);
 					SoundManager.playSound("SFX/S_Item_Get", "ItemGet", false, false);
 					this.ship2.getItemQueue().enque(item);
